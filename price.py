add29455cf2e128ab7792abd77a62a0018776326
Alina Magdalyuk <alina_magdalyuk@mail.ru>
2020-02-19 10:13:36 +0300

    Первая версия

---
+++ price.py
@@ -0,0 +1,13 @@
+def discounted(price, discount, max_discount=50):
+    price = abs(float(price))
+    discount = abs(float(discount))
+    if discount > max_discount:
+        price_with_discount = price
+    else:
+        price_with_discount = price - price * discount / 100
+    return price_with_discount
+
+product = {'name': 'Samsung Galaxy S10', 'stock': 8, 'price': 50000.0, 'discount': 55}
+
+product['with_discount'] = discounted(product['price'], product['discount'], max_discount=80)
+print(product)
